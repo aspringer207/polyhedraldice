fd02e37e8d6c29b609730bcbe0e0d739ffaa9be7
aspringer207 <69414777+aspringer207@users.noreply.github.com>
2020-08-09 14:31:35 -0400

    Update createdperc.py

--- createdperc.py
+++ createdperc.py
@@ -1,7 +1,5 @@
 bl_info = {
     "name": "Create Percentile Die",
-    "version": (1,0)
-    "blender": (2, 83, 0),
     "category": "Add",
     "author": "Andrea Springer"
     "description": "Creates a percentile die model from user prepared numerals or symbols.",
