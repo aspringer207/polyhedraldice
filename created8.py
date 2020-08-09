48c891ff293e620c8b2a208fe19a2d55aa3b74e4
aspringer207 <69414777+aspringer207@users.noreply.github.com>
2020-08-09 14:37:47 -0400

    Update created8.py

--- created8.py
+++ created8.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Create D8",
     "category": "Add",
-    "author": "Andrea Springer"
+    "author": "Andrea Springer",
     "description": "Creates a D8 model from user prepared numerals or symbols.",
     "tracker_url": "https://github.com/aspringer207/polyhedraldice",
     "support": "TESTING",
