29cada954349dd20c5efa5c4dd84014aa6b99fc6
aspringer207 <69414777+aspringer207@users.noreply.github.com>
2020-08-09 14:20:47 -0400

    Update created8.py

--- created8.py
+++ created8.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Create D8",
     "version": (1,0)
-    "blender": (2, 80, 0),
+    "blender": (2, 83, 0),
     "category": "Add",
     "author": "Andrea Springer"
     "description": "Creates a D8 model from user prepared numerals or symbols.",
