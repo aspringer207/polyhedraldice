a6e8e34a933ce1fcb9d4bbe2730d6ae5fae4b519
aspringer207 <69414777+aspringer207@users.noreply.github.com>
2020-08-09 14:19:37 -0400

    Update created4.py

--- created4.py
+++ created4.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Create D4",
     "version": (1,0)
-    "blender": (2, 80, 0),
+    "blender": (2, 83, 0),
     "category": "Add",
     "author": "Andrea Springer"
     "description": "Creates a D4 model from user prepared numerals or symbols.",
