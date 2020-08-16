e5386a85b96488a5bfa660085ae3b1c766793d68
aspringer207 <69414777+aspringer207@users.noreply.github.com>
2020-08-16 19:51:55 -0400

    Update created12.py

--- created12.py
+++ created12.py
@@ -202,6 +202,11 @@ class OBJECT_OT_create_d12(bpy.types.Operator):
         bpy.ops.object.modifier_add(type='BOOLEAN')
         bpy.context.object.modifiers["Boolean"].object = bpy.data.objects["5"]
         bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
+        
+        bpy.context.view_layer.objects.active = bpy.data.objects['Solid']        
+        for obj in bpy.context.selected_objects:
+            obj.name = "D12"
+            obj.data.name = "D12"        
        
         return {'FINISHED'}
 
