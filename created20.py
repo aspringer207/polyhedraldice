116e2b086f46eb6e683082aff5599d7a025e6855
aspringer207 <69414777+aspringer207@users.noreply.github.com>
2020-08-16 19:46:25 -0400

    Update created20.py

--- created20.py
+++ created20.py
@@ -356,6 +356,11 @@ class OBJECT_OT_create_d20(bpy.types.Operator):
         bpy.context.object.modifiers["Boolean"].object = bpy.data.objects["15"]
         bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Boolean")
         
+        bpy.context.view_layer.objects.active = bpy.data.objects['Solid']        
+        for obj in bpy.context.selected_objects:
+            obj.name = "D20"
+            obj.data.name = "D20"
+        
         return {'FINISHED'}
     
 def menu_draw(self, context):
